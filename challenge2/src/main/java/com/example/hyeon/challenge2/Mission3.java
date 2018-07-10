0892f78ed8e9495fee721d846ffe05783ac58cdd
Jason Jung <hyeonsuns123@gmail.com>
2018-07-11 08:56:07 +0900

    Today I studied Challenge Project 0711

---
+++ challenge2/src/main/java/com/example/hyeon/challenge2/Mission3.java
@@ -0,0 +1,13 @@
+package com.example.hyeon.challenge2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mission3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission3);
+    }
+}
