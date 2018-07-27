6228a32e27a4b9de81b969df534fa9fac9438765
Jason Jung <hyeonsuns123@gmail.com>
2018-07-27 16:14:08 +0900

    Today I studied Various View Mission4 0727

---
+++ challenge4/src/main/java/com/example/hyeon/challenge4/CallLogWrapper.java
@@ -0,0 +1,18 @@
+package com.example.hyeon.challenge4;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CallLogWrapper {
+    public ImageView personImageView;
+    public TextView nameView;
+    public TextView dateView;
+    public ImageView dialerImageView;
+    public CallLogWrapper(View root){
+        personImageView=(ImageView)root.findViewById(R.id.main_item_person);
+        nameView=(TextView)root.findViewById(R.id.main_item_name);
+        dateView=(TextView)root.findViewById(R.id.main_item_date);
+        dialerImageView=(ImageView)root.findViewById(R.id.main_item_dialer);
+    }
+}
---
+++ challenge4/src/main/java/com/example/hyeon/challenge4/CallerVO.java
@@ -0,0 +1,8 @@
+package com.example.hyeon.challenge4;
+
+public class CallerVO {
+    public String name;
+    public String photo;
+    public String date;
+    public String phone;
+}
